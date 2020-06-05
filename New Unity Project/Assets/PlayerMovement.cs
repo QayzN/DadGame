f7d646454b51c39f74d828f8e743f82fca62104d
Qayz Nurani <31449655+QayzN@users.noreply.github.com>
2020-06-04 22:39:32 -0700

    DadGame 0.5
    Added shooting, particle effects, moevement. Just need enemies that chase and score system (plus menu).

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+
+        //W
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir += Vector3.up;
+        }
+
+        //A
+        if (Input.GetKey(KeyCode.A))
+        {
+            dir += Vector3.left;
+        }
+
+        //S
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir += Vector3.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir += Vector3.right;
+        }
+
+        this.transform.Translate(dir.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ New Unity Project/Assets/RandomSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawn : MonoBehaviour
+{
+
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > nextSpawn)
+        {
+
+            nextSpawn = Time.deltaTime + spawnRate;
+            randX = Random.Range(-8f, 8f);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
